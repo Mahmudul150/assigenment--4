3a362974e5bf7d55fd0ad9eb3a13368567c59131
Mahmudul Hasan <mahmudul61@gmail.com>
2026-07-16 23:45:16 +0600

    globalerrorhandling ar modde errorDetails setup

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -8,61 +8,90 @@ export const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log("Error:", err);
 
-  let statusCode ;
+  
+
+  let statusCode:number = httpStatus.INTERNAL_SERVER_ERROR;
   let errorMessage = err.message || "Internal Server Error";
-  let errorName = err.name || "Error";
   let errorDetails: any = null;
 
+
   if (err instanceof Prisma.PrismaClientValidationError) {
+
     statusCode = httpStatus.BAD_REQUEST;
-    errorMessage = "You have provided incorrect field type or missing fields";
+    errorMessage =
+      "You have provided incorrect field type or missing fields";
+
     errorDetails = err.message;
+
 
   } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
 
+
     if (err.code === "P2002") {
+
       statusCode = httpStatus.CONFLICT;
       errorMessage = "Duplicate Key Error";
       errorDetails = err.meta;
 
+
     } else if (err.code === "P2003") {
+
       statusCode = httpStatus.BAD_REQUEST;
       errorMessage = "Foreign key constraint failed";
       errorDetails = err.meta;
 
+
     } else if (err.code === "P2025") {
+
       statusCode = httpStatus.NOT_FOUND;
-      errorMessage =
-        "Required record was not found.";
+      errorMessage = "Required record was not found.";
+
     }
+
 
   } else if (err instanceof Prisma.PrismaClientInitializationError) {
 
+
     if (err.errorCode === "P1000") {
+
       statusCode = httpStatus.UNAUTHORIZED;
       errorMessage =
         "Authentication failed against database server.";
 
     } else if (err.errorCode === "P1001") {
+
       statusCode = httpStatus.SERVICE_UNAVAILABLE;
-      errorMessage = "Can't reach database server.";
+      errorMessage =
+        "Can't reach database server.";
+
     }
 
+
   } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+
+
     statusCode = httpStatus.INTERNAL_SERVER_ERROR;
-    errorMessage = "Error occurred during query execution";
+    errorMessage =
+      "Error occurred during query execution";
+
 
   } else if (err instanceof Error) {
+
+
     statusCode = httpStatus.INTERNAL_SERVER_ERROR;
     errorMessage = err.message;
+
   }
- res.status(statusCode || httpStatus.INTERNAL_SERVER_ERROR).json({
-        success: false,
-        statusCode: statusCode || httpStatus.INTERNAL_SERVER_ERROR,
-        name : errorName,
-        message: errorMessage,
-        error: err.stack
-    })
+
+
+  res.status(statusCode).json({
+
+    success: false,
+    statusCode,
+    message: errorMessage,
+    errorDetails
+
+  });
+
 };
