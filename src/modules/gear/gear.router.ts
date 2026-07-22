3534d0cdc51633ddef45531d5b75811ec14aa726
Mahmudul150 <hasan5016879@gmail.com>
2026-07-22 23:43:45 +0600

    fix gear service

--- src/modules/gear/gear.router.ts
+++ src/modules/gear/gear.router.ts
@@ -6,9 +6,6 @@ const router = Router()
 
 router.get('/', gearController.getAllGear);
 router.get('/categories/:categoryId',gearController.getGearWithCategory)
-router.get("/categories/test", (req:Request, res:Response) => {
-  res.json({ message: "categories route working" });
-});
 router.get('/:id',gearController.getSingelGear)
 
 
