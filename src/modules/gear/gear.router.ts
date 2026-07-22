5b5d1b92d45c9665989a71e4717c1b45113e1867
Mahmudul150 <hasan5016879@gmail.com>
2026-07-22 21:10:15 +0600

    Add debug route for category

--- src/modules/gear/gear.router.ts
+++ src/modules/gear/gear.router.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { gearController } from "./gear.controler";
 
 const router = Router()
@@ -6,6 +6,9 @@ const router = Router()
 
 router.get('/', gearController.getAllGear);
 router.get('/categories/:categoryId',gearController.getGearWithCategory)
+router.get("/categories/test", (req:Request, res:Response) => {
+  res.json({ message: "categories route working" });
+});
 router.get('/:id',gearController.getSingelGear)
 
 
